e4dde86a96abc6ed682e15fea93f61fa54ac515d
Luis Saca <sacaluisv@gmail.com>
2024-09-21 11:09:00 -0400

    uno

--- 01.py
+++ 01.py
@@ -29,8 +29,21 @@ class NumeroEspecial:
 def main():
   # ingresar un lote de numeros en el intervalo de [x, y]. el algoritmo principal debe pedir la entrada de datos en x un valor igual o superior a 100 de lo contrario finaliza el algoritmo. en la entrada de datos en y debe ser un valor superios a la entrada de datos x, de lo contrario tambien finaliza el algoritmo. dentro el intervalo de lote de numero [x, y], hacer un barrido mostrando los numero sprimos y capicuas llamando a la clase NumeroEspecial y aplicando sus metodos
   
+  x = int(input("Introduzca el valor de X: "))
+  # x debe ser mayor a 100
+  while x < 100:
+    x = int(input("Introduzca el valor de X: "))
+  y = int(input("Introduzca el valor de Y: "))
+  # y debe ser mayor a x
+  while y <= x:
+    y = int(input("Introduzca el valor de Y: "))
   
-  
-  
+  for i in range(x, y + 1):
+    numero = NumeroEspecial(i)
+    if numero.es_primo():
+      print(f"PRIMO {i} ")
+    if numero.es_capicua():
+      print(f"CAPICUA {i}")
+      
   
 main()
